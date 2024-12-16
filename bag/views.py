80bff458c25b7b822e5be99a747c6da6b3eda65f
DaveWhiteh <david_whiteh@msn.com>
2024-12-16 22:44:10 +0000

    Add messages to views and update toast_error.html

--- bag/views.py
+++ bag/views.py
@@ -11,7 +11,7 @@ def view_bag(request):
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
-    """ Add a specified product to the shopping bag """
+    """ Add a specified product to the shopping basket """
 
     product = get_object_or_404(Product, pk=item_id)
     quantity = 1
@@ -19,10 +19,10 @@ def add_to_bag(request, item_id):
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+        messages.error(request, f'{product.model_name} already exists in your basket. Please choose another product.')
     else:
         bag[item_id] = quantity
-        messages.success(request, f'Added {product.model_name} to your bag')
+        messages.success(request, f'Added {product.model_name} to your basket.')
 
     request.session['bag'] = bag
     # For Test Purposes
@@ -31,16 +31,18 @@ def add_to_bag(request, item_id):
 
 
 def remove_from_bag(request, item_id):
-    """ Remove the item from the shopping bag """
+    """ Remove the item from the shopping basket """
 
     try:
         product = get_object_or_404(Product, pk=item_id)
         bag = request.session.get('bag', {})
 
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.model_name} from your basket.')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
