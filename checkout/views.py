7120c599d3557d10b608cd00610aa7948f2c271b
DaveWhiteh <david_whiteh@msn.com>
2024-12-18 01:18:52 +0000

    Added Stripe payment functionality

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Pc5ImCwIwEettPLLrVzAMSAmJ21GmqupXHKS3xIysSzSSQLIStIKrnBHxKE2GNbtQPiB7De3jCZIO4P7aFKK5jo00vphJzDfu',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
