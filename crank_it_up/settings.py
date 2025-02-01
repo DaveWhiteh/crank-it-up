ef07532700b336e59d09959e33e820607b434ddf
DaveWhiteh <david_whiteh@msn.com>
2025-02-01 23:14:41 +0000

    Setup postgres db and heroku app

--- crank_it_up/settings.py
+++ crank_it_up/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,10 +126,9 @@ WSGI_APPLICATION = 'crank_it_up.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
